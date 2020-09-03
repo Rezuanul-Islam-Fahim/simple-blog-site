53befdfa0d24d00410cf57d7d673836ac8c9fa61
Rezuanul Islam Fahim <rifahim98@gmail.com>
2020-09-03 23:52:01 +0600

    Add delete post functionality

--- website/blog.py
+++ website/blog.py
@@ -90,3 +90,13 @@ def update(id):
         if (error): flash(error)
 
     return render_template('blog/update.html', post=post)
+
+
+@bp.route('/delete/<int:id>', methods=['POST'])
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', [id])
+    db.commit()
+    return redirect(url_for('blog.index'))
