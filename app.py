6d0cd2876941efea82cde3fb80f3aafc2c11e3b4
Rezuanul Islam Fahim <rifahim98@gmail.com>
2020-09-22 13:54:44 +0600

    Change main py file's structure

--- app.py
+++ app.py
@@ -1,31 +1,25 @@
 import os
 from flask import Flask
 
+import database
+import auth
+import blog
 
-def create_app():
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='DEV',
-        DATABASE=os.path.join(app.instance_path, 'data.sqlite')
-    )
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='DEV',
+    DATABASE=os.path.join(app.instance_path, 'data.sqlite'),
+)
 
-    @app.route('/test')
-    def test():
-        return 'This is just a test'
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    import database
-    database.init_app(app)
+database.init_app(app)
 
-    import auth
-    app.register_blueprint(auth.bp)
+app.register_blueprint(auth.bp)
 
-    import blog
-    app.register_blueprint(blog.bp)
-    app.add_url_rule('/', 'index')
-
-    return app
+app.register_blueprint(blog.bp)
+app.add_url_rule('/', 'index')
