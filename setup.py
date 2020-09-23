f6e07a434ef03aa5f625fe13dad117cdd3429d90
Rezuanul Islam Fahim <rifahim98@gmail.com>
2020-09-23 19:48:34 +0600

    Update setup.py and .gitignore file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 # Setup information
 setup(
-    name='Simple-Blog-Site',
+    name='simple-blog-site',
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
